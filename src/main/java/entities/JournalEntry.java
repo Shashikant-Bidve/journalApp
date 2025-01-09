1a48dddb4246452ef5d4c8c8d08a32bdfed06029
Shashi <shashi.bidve18@gmail.com>
2025-01-09 23:52:59 +0530

    db connected

--- src/main/java/entities/JournalEntry.java
+++ src/main/java/entities/JournalEntry.java
@@ -1,11 +1,24 @@
 package entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Getter
 @Setter
+@Document(collation = "journal_entries")
 public class JournalEntry {
-    private long id;
+    @Id
+    private ObjectId id;
 
     private String title;
 
     private String content;
+
+    private LocalDateTime date;
 }
